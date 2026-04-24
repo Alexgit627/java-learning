387e4663cd63fca31326a3fef85074cb13a9bc52
Александр <rozovik150305@gmail.com>
2026-04-24 21:06:44 +0300

    learning basic things

---
+++ src/week_1/Day1.java
@@ -0,0 +1,30 @@
+package week_1;
+
+public class Day1 {
+    public static void main(String[] args) {
+        String name = "Alex";
+        int age = 20;
+        double height = 175.5;
+        boolean isStudent = true;
+        int a = 10;
+        int b = 5;
+        System.out.println("Sum: " + (a+b));
+        System.out.println("Difference: " + (a -b));
+        System.out.println("Multiplication: " + (a*b));
+        System.out.println("Division: " + (a/b));
+        double price = 99.99;
+        System.out.println("Price is: " + price);
+        int seconds = 125;
+        System.out.println((125/60) + " minutes " + (125%60) + " seconds");
+        double Price = 125.5;
+        int count = 3;
+        double total = Price * count;
+        System.out.println("Total price is: " + total);
+        int money = 5678;
+        System.out.println("Thousands: " +(5678/1000) + " Hundreds " + (5678/100));
+        System.out.println("Tens " + (5678/10%10) + " Units " + (5678%10));
+
+
+
+    }
+}
