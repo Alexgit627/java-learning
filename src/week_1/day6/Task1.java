7c5faf315459fb0c2fc6c5bf982430db2a77474a
Александр <rozovik150305@gmail.com>
2026-05-02 14:37:06 +0300

    cycles while,do while,completed

---
+++ src/week_1/day6/Task1.java
@@ -0,0 +1,22 @@
+package week_1.day6;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+
+        while (n <= 10 ) {
+            if (n == 5) {
+                n++;
+                continue;
+
+            }
+            System.out.println(n);
+            n++;
+
+        }
+    }
+}
