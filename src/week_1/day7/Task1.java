c585530bfbdb2cf2ff49ef530d218d9e0db88256
Александр <rozovik150305@gmail.com>
2026-05-04 16:23:16 +0300

    simples  arrays completed

---
+++ src/week_1/day7/Task1.java
@@ -0,0 +1,21 @@
+package week_1.day7;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Размер массива введи: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length;i++) {
+            System.out.println("Введи значениe: ");
+            arr[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < arr.length;i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
---
+++ src/week_1/day7/Task4.java
@@ -0,0 +1,43 @@
+package week_1.day7;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for ( int i = 0; i < arr.length;i++){
+            System.out.print("Enter number: ");
+            arr[i] = scanner.nextInt();
+
+        }
+        int count = 0;
+        for (int i = 0; i < arr.length;i++) {
+            if (arr[i] > 0) {
+                count++;
+            }
+        }
+        System.out.println("Positive: " + count);
+
+        int neg = 0;
+        for(int i = 0; i < arr.length;i++) {
+            if (arr[i] < 0) {
+                neg++;
+            }
+        }
+        System.out.println("Negative: " + neg);
+
+        int zero = 0;
+        for( int i = 0;  i < arr.length;i++) {
+            if (arr[i]==0) {
+                zero++;
+            }
+        }
+        System.out.println("Zero: " + zero);
+
+    }
+}
