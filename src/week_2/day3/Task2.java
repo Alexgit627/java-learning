aaa3c580e994361e8aa4612ea2b4d9f8c26b5abb
Александр <rozovik150305@gmail.com>
2026-06-08 16:45:03 +0300

    day 3 of second week completed

---
+++ src/week_2/day3/Task2.java
@@ -0,0 +1,13 @@
+package week_2.day3;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int result = numbers(2,4);
+        System.out.println(result);
+    }
+
+    public static int numbers(int a, int b) {
+        return a + b;
+
+    }
+}
